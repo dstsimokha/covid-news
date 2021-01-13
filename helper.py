4779c0ef225235b3f85c366ad52d23f848ebdf09
dstsimokha <dstsimokha@gmail.com>
2021-01-13 15:06:49 +0300

    Remove helper script

--- helper.py
+++
@@ -1,7 +0,0 @@
-from scraper import *
-news = Scraper('kp')
-news.test_parse()
-print('URL', news.url, sep='\n')
-print('TITLE', news.title, sep='\n')
-print('TIME', news.time, sep='\n')
-print('TEXT', news.text, sep='\n')
